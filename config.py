3ed32ff696dd8222dda51881e7823eecc33694d7
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-12-07 21:37:47 +0900

    Update MySQL password in config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'      # 본인의 MySQL 사용자명
-    MYSQL_PASSWORD = '0224'  # 본인의 MySQL 비밀번호
+    MYSQL_PASSWORD = '0'  # 본인의 MySQL 비밀번호
     MYSQL_DB = 'reservation_system'
     SECRET_KEY = os.urandom(24)
